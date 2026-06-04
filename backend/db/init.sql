cfde6465994022c8efb8bd90b4068f1e653130c5
firecx <nikandrov.alekseix1@gmail.com>
2026-06-04 18:14:02 +0600

    база для бд

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -1,34 +1,38 @@
--- Таблица авторов
-CREATE TABLE IF NOT EXISTS authors (
-    author_id SERIAL PRIMARY KEY,
-    author_name VARCHAR(100) UNIQUE NOT NULL
+-- Таблица разработчиков
+CREATE TABLE IF NOT EXISTS developers (
+    id SERIAL PRIMARY KEY,
+    dName VARCHAR(100) UNIQUE NOT NULL
 );
 
--- Таблица книг (связь с автором)
-CREATE TABLE IF NOT EXISTS books (
-    book_id SERIAL PRIMARY KEY,
-    book_title VARCHAR(200) NOT NULL,
-    book_cover_url VARCHAR(500),
+-- Таблица издателей
+CREATE TABLE IF NOT EXISTS publishers (
+    id SERIAL PRIMARY KEY,
+    pName VARCHAR(100) UNIQUE NOT NULL
+);
+
+-- Таблица игр
+CREATE TABLE IF NOT EXISTS games (
+    id SERIAL PRIMARY KEY,
+    gName VARCHAR(200) NOT NULL,
+    cover_url VARCHAR(500),
+    rating VARCHAR(4),
+    release_date DATE,
+    likes INT,
+    price FLOAT,
     annotation TEXT,
-    table_of_contents TEXT,
-    author_id INTEGER NOT NULL REFERENCES authors(author_id) ON DELETE CASCADE
+    system_requirements TEXT,
+    developer_id INTEGER NOT NULL REFERENCES developers(id) ON DELETE CASCADE,
+    publisher_id INTEGER NOT NULL REFERENCES publishers(id) ON DELETE CASCADE
 );
 
--- Таблица жанров и связь многие-ко-многим
+-- Таблица жанров
 CREATE TABLE IF NOT EXISTS genres (
-    genre_id SERIAL PRIMARY KEY,
-    genre_name VARCHAR(100) UNIQUE NOT NULL
+    id SERIAL PRIMARY KEY,
+    gName VARCHAR(100) UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS books_genres (
-    book_id INTEGER NOT NULL REFERENCES books(book_id) ON DELETE CASCADE,
-    genre_id INTEGER NOT NULL REFERENCES genres(genre_id) ON DELETE CASCADE,
-    PRIMARY KEY (book_id, genre_id)
+CREATE TABLE IF NOT EXISTS games_genres (
+    game_id INTEGER NOT NULL REFERENCES games(id) ON DELETE CASCADE,
+    genre_id INTEGER NOT NULL REFERENCES genres(id) ON DELETE CASCADE,
+    PRIMARY KEY (game_id, genre_id)
 );
-
--- Индексы для ускорения
-CREATE INDEX IF NOT EXISTS idx_authors_name ON authors(author_name);
-CREATE INDEX IF NOT EXISTS idx_books_author_id ON books(author_id);
-CREATE INDEX IF NOT EXISTS idx_books_genres_book_id ON books_genres(book_id);
-CREATE INDEX IF NOT EXISTS idx_books_genres_genre_id ON books_genres(genre_id);
-CREATE INDEX IF NOT EXISTS idx_genres_name ON genres(genre_name);
